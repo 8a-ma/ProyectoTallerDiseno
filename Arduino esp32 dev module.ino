107e01c21e436c8f23870bdfb4899892fe183f6d
8a-ma <81239922+8a-ma@users.noreply.github.com>
2022-11-19 09:33:40 -0300

    Version 3

--- Arduino esp32 dev module.ino
+++ Arduino esp32 dev module.ino
@@ -1,11 +1,17 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <TimeLib.h>
+
+LiquidCrystal_I2C lcd(0x20, 16, 4);
 
 // sensorUltra1
 int TriggPin = 17;
 int EcchoPin = 5;
-int V5 = 23;
+int V5 = 21;
 
 //sensorUltra2
+int TriggPin2 = 23;
+int EcchoPin2 = 22;
 
 int LDRPines[2] = {12, 13};
 int V5Pines[2]  = {14, 27};
@@ -16,6 +22,7 @@ int columnas = 2;
 int matrizLDR[2][2] = {{0, 0}, {0, 0}};
 
 int estDesc = filas + columnas;
+int valuePrint;
 
 void loopMatrizLDR(){
   for(int i = 0; i <= columnas -1; i++) // n-1, donde n es el numero de columnas
@@ -61,7 +68,7 @@ int valueMatrizLDR(){
 }
 
 bool sensorUltra1(){
-  digitalWrite(V5, HIGH);
+  
 
   int difTiempo;
   int distancia;
@@ -78,15 +85,36 @@ bool sensorUltra1(){
     return true;
   }
   else{
-    return false,
+    return false;
   }
 }
 
-void setup()
-{
+bool sensorUltra2(){
+
+  int difTiempo;
+  int distancia;
+  
+  digitalWrite(TriggPin2, HIGH);
+  delay(1);
+  digitalWrite(TriggPin2, LOW);
+  
+  difTiempo = pulseIn(EcchoPin2, HIGH);
+  distancia = difTiempo / 58.2;
+  delay(200);
+
+  if(distancia <=200 && distancia >= 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+void setup(){
   
   pinMode(EcchoPin, INPUT);
   pinMode(TriggPin, OUTPUT);
+  pinMode(EcchoPin2, INPUT);
+  pinMode(TriggPin2, OUTPUT);
   pinMode(V5, OUTPUT);
   
   pinMode(V5Pines[0], OUTPUT);
@@ -94,12 +122,21 @@ void setup()
 
   pinMode(LDRPines[0], INPUT);
   pinMode(LDRPines[1], INPUT);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Hola Mundo");
+  lcd.setCursor(0,1);
   
+  delay(10);
+
   Serial.begin(9600); 
 }
 
-void loop()
-{ 
+void loop(){ 
+
+  digitalWrite(V5, HIGH);
+ /*
   if(sensorUltra1()){
 
     estDesc = estDesc -1;
@@ -107,37 +144,31 @@ void loop()
     if (estDesc < 0){
       estDesc = 0;
     }
-/*
-    int matrizCopy[2][2];
-
-    memcpy(matrizCopy, matrizLDR, sizeof(matrizLDR));    
-
-    for(int t = 0; t <= 5; t++)
-    {
-      loopMatrizLDR();
-
-      if(memcmp(matrizLDR, matrizCopy, sizeof(matrizLDR)) != 0)
-      {
-        break;
-      }
-      else
-      {
-        delay(60000);
-      }      
-    }
-*/
   }
 
-  //sensorUltra2() => estDesc = estDesc +1;
+  if(sensorUltra2()){
 
-  if (minute() % 2 == 0){
+    estDesc = estDesc +1;
+
+    if (estDesc > 4){
+      estDesc = 4;
+    }
+  }
+
+  valuePrint = estDesc;
+
+  if (second() % 5 == 0 || second() == 0){
 
     if (estDesc != valueMatrizLDR()){ //Se verifica que sean iguales para luego cambiarlo si es que no son iguales con lo real
-      Serial.println(valueMatrizLDR()); //Se print lo que es real
-      estOcup = valueMatrizLDR();
-      }
+      valuePrint = valueMatrizLDR(); //Se print lo que es real
+      estDesc = valueMatrizLDR();
     }
+
+   }
  
-  Serial.println(estDesc); //Se print primero los estacionamientos "desocupados"
+  Serial.println(valuePrint); //Se print primero los estacionamientos "desocupados"
+  lcd.setCursor(0,1);
+  lcd.print(String(valuePrint));
+  */
   
 }
